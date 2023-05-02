94164f37de3e4c022e572e8c1448fe03ccfbc90f
Jessica Stevens <jessicalynncodes@gmail.com>
2023-05-02 01:42:27 -0500

    I remembered how to include images

--- react-facts/src/components/Navbar.jsx
+++ react-facts/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import logo from '/assets/logo.svg';
+
 export default function Navbar() {
 	return (
 		<nav>
-			<img src="./assets/logo.svg" />
+			<img src={logo} />
 			<h2>React Course - Project 1</h2>
 		</nav>
 	)
